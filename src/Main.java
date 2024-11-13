2bd55425c9b9903c0953cf454eff3254296fccf7
huawei <romanec.aleksey@gmail.com>
2024-11-13 23:20:12 +0400

    HW3.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work-3");
+
+        System.out.println("Задание-1");
+        byte apple = 1;
+        int orange= 23;
+        short pepper=2000;
+        long tomato=1000000;
+        float sugar=0.5f;
+        double flour=4.5;
+        System.out.println("Яблоко="+apple+"шт; "+"апельсин="+orange+"шт; "+"перец="+pepper+"шт; "+"помидор="+tomato+"шт; "+"сахар="+sugar+"кг; "+"мука="+flour+"кг.");
     }
  }
