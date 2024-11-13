6a7c997072c0eea60e899d49863bdd310dcd700b
huawei <romanec.aleksey@gmail.com>
2024-11-14 00:24:29 +0400

    HW3.0

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,22 @@ public class Main {
 
         System.out.println("Задание-1");
         byte apple = 1;
-        int orange= 23;
-        short pepper=2000;
-        long tomato=1000000;
-        float sugar=0.5f;
-        double flour=4.5;
-        System.out.println("Яблоко="+apple+"шт; "+"апельсин="+orange+"шт; "+"перец="+pepper+"шт; "+"помидор="+tomato+"шт; "+"сахар="+sugar+"кг; "+"мука="+flour+"кг.");
+        int orange = 23;
+        short pepper = 2000;
+        long tomato = 1000000;
+        float sugar = 0.5f;
+        double flour = 4.5;
+        System.out.println("Яблоко=" + apple + "шт; " + "апельсин=" + orange + "шт; " + "перец=" + pepper + "шт; " + "помидор=" + tomato + "шт; " + "сахар=" + sugar + "кг; " + "мука=" + flour + "кг.");
+        System.out.println();
+
+        System.out.println("Задание-2");
+        float a = 27.12f;
+        long b = 987678965549L;
+        float c = 2.786f;
+        short d = 569;
+        short e = -159;
+        short f = 27897;
+        byte g = 67;
+        System.out.println(" float=" + a + " long=" + b + " float=" + c + " short=" + d + " short=" + e + " short=" + f + " byte=" + g);
     }
- }
+}
